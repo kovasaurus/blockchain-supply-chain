5a9d928552cd692bc6cdf69d6edede5c5d2eec30
Ivan Kovacic <ivan.kovacic@barrage.net>
2021-07-13 16:03:03 +0200

    init commit supply chain

---
+++ README.md
@@ -0,0 +1,10 @@
+contract address: 0x90423749ED9fe1957E312f4087A15152ceC26Dff
+transaction: 0xac46c8e681aa14edb52265c3fd184373b8d030ea48dab94a29b5836fb79ae79e
+
+Truffle v5.3.10 (core: 5.3.10)
+Solidity - 0.8.0 (solc-js)
+Node v14.16.0
+Web3.js v1.3.6
+
+Third party Libraries: 
+truffle-hdwallet-provider version: ^1.0.17
---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+const HDWallet = require('truffle-hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+      websockets: true
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWallet(mnemonic, "https://rinkeby.infura.io/v3/7c59362d31d14176b6ba545979cdb49d", 0);
+      },
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
